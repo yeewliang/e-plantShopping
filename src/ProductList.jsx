1be9da8c77b9a4c7f89c0e9b34cb5bb53ee916a9
Wei Liang <yeewliang@gmail.com>
2025-03-20 02:43:19 -0400

    Added product card

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -274,6 +274,22 @@ function ProductList({ onHomeClick }) {
             </div>
             {!showCart ? (
                 <div className="product-grid">
+                    {plantsArray.map((category, index) => (
+                        <div key={index}>
+                            <h1><div>{category.category}</div></h1>
+                            <div className='product-list'>
+                                {category.plants.map((plant, plantIndex) => (
+                                <div className='product-card' key={plantIndex}>
+                                    <img className='product-image' src={plant.image} alt={plant.name}/>
+                                    <div className='product-title'>{plant.name}</div>
+                                    <div className='product-description'>{plant.description}</div>
+                                    <div className='product-price'>{plant.cost}</div>   
+                                    <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button> 
+                                </div>
+                                ))}
+                            </div>
+                        </div>
+                    ))}
 
 
                 </div>
